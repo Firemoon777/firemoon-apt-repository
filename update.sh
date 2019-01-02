53ab35e71765ce47aa09c812761119baddb8f574
Firemoon777 <firemoon@icloud.com>
2019-01-02 08:41:28 -0500

    fixed update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-apt-ftparchive packages public_html/packages > Packages
-apt-ftparchive release public_html > Release
+apt-ftparchive packages public_html/packages > public_html/Packages
+apt-ftparchive release public_html > public_html/Release
