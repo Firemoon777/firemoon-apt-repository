1c905e6ebb0a21980313ea8f18e1ae73b2dfe900
Firemoon777 <firemoon@icloud.com>
2019-01-02 08:26:16 -0500

    moving to public_html

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-apt-ftparchive packages packages > Packages
-apt-ftparchive release . > Release
+apt-ftparchive packages public_html/packages > Packages
+apt-ftparchive release public_html > Release
